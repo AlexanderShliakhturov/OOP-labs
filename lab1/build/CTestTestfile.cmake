5857557ef6a2d670ec1485b468777224ef5234b2
alexander <shliakhturov@gmail.com>
2023-09-21 18:14:30 +0300

    еще исправления

---
+++ lab1/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/alexander/labsoop/lab1
+# Build directory: /home/alexander/labsoop/lab1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(BASIC "/home/alexander/labsoop/lab1/build/tests")
+set_tests_properties(BASIC PROPERTIES  _BACKTRACE_TRIPLES "/home/alexander/labsoop/lab1/CMakeLists.txt;16;add_test;/home/alexander/labsoop/lab1/CMakeLists.txt;0;")
