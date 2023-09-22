f6f474ad349e7a5ae32ed4cd41b162cd70eb5f0c
alexander <shliakhturov@gmail.com>
2023-09-22 14:52:33 +0300

    последние изменения

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -5,27 +5,25 @@
 
 std::pair<int, int> closest_pair_tonum(int n)
 {
-    std::pair<int, int> answer;
-    bool is_sum_square;
-    bool is_diff_square;
+    std::pair<int, int> answer;;
 
     for (int i = n; i > 0; i--)
     {
         for (int j = n - 1; j > 0; j--)
         {
-            is_sum_square = sqrt(i + j) == int(sqrt(i + j));
-            is_diff_square = (sqrt(i - j) == int(sqrt(i - j)));
+            bool is_sum_square = sqrt(i + j) == int(sqrt(i + j));
+            bool is_diff_square = (sqrt(i - j) == int(sqrt(i - j)));
+            bool is_finded = i > j 
+                && is_sum_square 
+                && is_diff_square 
+                && i < n;
 
-            if (i > j && is_sum_square && is_diff_square && (i < n))
+            if (is_finded)
             {
-                answer.first = i;
-                answer.second = j;
-                return answer;
+                return {i, j};
             }
         }
     }
-    
-    answer.first = 0;
-    answer.second = 0;
-    return answer;
+
+    return {0, 0};
 }
