4d2a0a3f06c1aeb35a7226daf60967f543ef5222
alexander <shliakhturov@gmail.com>
2023-09-30 22:58:45 +0300

    second

--- lab2/build/lib/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alexander/labsoop/lab2/lib/googletest/googletest
-# Build directory: /home/alexander/labsoop/lab2/build/lib/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
